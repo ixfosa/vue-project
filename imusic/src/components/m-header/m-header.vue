571c86a22d2d3226080388f105c1e5c5595de3ec
ixfosa <ixfosa@163.com>
2021-08-01 16:05:39 +0800

    tab

--- imusic/src/components/m-header/m-header.vue
+++ imusic/src/components/m-header/m-header.vue
@@ -20,20 +20,18 @@ export default {
   height: 45px
   text-align: center
   color: $color-theme
-
-.icon
-  display: inline-block;
-  vertical-align: top
-  margin-top: 6px
-  width: 30px
-  height: 32px
-  margin-right: 3px
-  bg-image('logo')
-  background-size: 30px
-
-.text
-  display: inline-block
-  vertical-align: top
-  line-height: 44px
-  font-size: $font-size-large
+  .icon
+    display: inline-block;
+    vertical-align: top
+    margin-top: 6px
+    width: 30px
+    height: 32px
+    margin-right: 3px
+    bg-image('logo')
+    background-size: 30px
+  .text
+    display: inline-block
+    vertical-align: top
+    line-height: 44px
+    font-size: $font-size-large
 </style>
